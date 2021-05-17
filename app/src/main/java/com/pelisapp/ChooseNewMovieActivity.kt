9395f5911bfde174cf884c469260189c55dbb02e
Martin Salazar <martin.salazar20@gmail.com>
2021-05-17 18:08:59 -0300

    Choose new movie activity (basic UI)

---
+++ app/src/main/java/com/pelisapp/ChooseNewMovieActivity.kt
@@ -0,0 +1,12 @@
+package com.pelisapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ChooseNewMovieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_choose_new_movie)
+    }
+}
