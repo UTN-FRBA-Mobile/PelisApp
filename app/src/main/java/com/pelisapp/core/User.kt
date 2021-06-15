bc50b37a8bfb3239d0f5ca2e766da489c0fccf0f
Martin Gabriel Salazar <martin.salazar20@gmail.com>
2021-06-14 23:16:45 -0300

    My groups activity (#5)
    * Added usergroup_item layout
    
    * Added groups information binding to activity_my_groups.xml

--- app/src/main/java/com/pelisapp/core/User.kt
+++ app/src/main/java/com/pelisapp/core/User.kt
@@ -2,20 +2,5 @@ package com.pelisapp.core
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
-data class User(val id: Int, val name: String) {
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as UserGroup
-
-        if (id != other.id) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return id
-    }
+class User(id: Int, val name: String) : Entity(id) {
 }
--- app/src/main/java/com/pelisapp/core/UserGroupApi.kt
+++ app/src/main/java/com/pelisapp/core/UserGroupApi.kt
@@ -13,9 +13,13 @@ class UserGroupApi {
         val nicolasMockUser = User(4, "Nicolas")
         val martinianoMockUser = User(5, "Martiniano")
 
-        val marvelFansGroup = UserGroup(1, "Marvel Fans", listOf(martinMockUser, lucasMockUser, javierMockUser))
-        val tarantinoFansGroup = UserGroup(2, "Tarantino Fans", listOf(lucasMockUser, nicolasMockUser, martinianoMockUser) )
-        val jamesCameronFansGroup = UserGroup(3, "James Cameron Fans", listOf(martinMockUser, martinianoMockUser))
+        val marvelImageLink = "https://www.treehugger.com/thmb/m7asEl8wPZJaZzmv6oCEjS3dOXc=/300x300/smart/filters:no_upscale()/__opt__aboutcom__coeus__resources__content_migration__mnn__images__2013__02__spiderman-7fb58f51ca0b4874a136e3951d367a9a.jpg"
+        val tarantinoImageLink = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Quentin_Tarantino_by_Gage_Skidmore.jpg/220px-Quentin_Tarantino_by_Gage_Skidmore.jpg"
+        val jamesCameronImageLink = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/JamesCameronHWOFOct2012.jpg/300px-JamesCameronHWOFOct2012.jpg"
+
+        val marvelFansGroup = UserGroup(1, "Marvel Fans", marvelImageLink, listOf(martinMockUser, lucasMockUser, javierMockUser))
+        val tarantinoFansGroup = UserGroup(2, "Tarantino Fans", tarantinoImageLink, listOf(martinMockUser, lucasMockUser, nicolasMockUser, martinianoMockUser) )
+        val jamesCameronFansGroup = UserGroup(3, "James Cameron Fans", jamesCameronImageLink, listOf(martinMockUser, martinianoMockUser))
 
         return listOf(marvelFansGroup, tarantinoFansGroup, jamesCameronFansGroup)
     }
