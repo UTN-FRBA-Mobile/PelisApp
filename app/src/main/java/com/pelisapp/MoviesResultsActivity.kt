c17b08d519418959088426841cff09a6f2022986
mbaridonn <mbaridonn@gmail.com>
2021-07-05 01:59:20 -0300

    Results are displayed if all the users have entered their preferences

--- app/src/main/java/com/pelisapp/MoviesResultsActivity.kt
+++ app/src/main/java/com/pelisapp/MoviesResultsActivity.kt
@@ -2,11 +2,9 @@ package com.pelisapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.pelisapp.core.*
-import com.pelisapp.ui.dashboard.MoviesAdapter
+import com.pelisapp.ui.results.MoviesResultsFragment
+import com.pelisapp.ui.results.WaitingForParticipantsFragment
 
 class MoviesResultsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,32 +12,19 @@ class MoviesResultsActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_movies_results)
 
-        val rvMovies = findViewById<View>(R.id.results_recycler_view) as RecyclerView
+        PreferencesApi().getAllPreferencesFromFirebase(object: PreferencesListener {
+            override fun onPreferencesReceived(preferences: HashMap<String, Preference>?) {
 
-        rvMovies.layoutManager = LinearLayoutManager(this)
-
-        MovieApi().getAllMoviesFromFirebase(object: MoviesListener{
-            override fun onMoviesReceived(movies: List<Movie>?) {
-                PreferencesApi().getAllPreferencesFromFirebase(object: PreferencesListener {
-                    override fun onPreferencesReceived(preferences: HashMap<String, Preference>?) {
-                        var directors = preferences!!.values.toList().map { pref -> pref.director }
-                        var genres = preferences!!.values.toList().map { pref -> pref.genre }
-
-                        var chosenDirector = getMostRepeatedWord(directors)
-                        var chosenGenre = getMostRepeatedWord(genres)
-
-                        var chosenMovies = movies!!.filter { movie -> movie.director.equals(chosenDirector) && movie.genre.equals(chosenGenre) }
-
-                        val adapter = MoviesAdapter(chosenMovies)
-
-                        rvMovies.adapter = adapter
-                    }
-                })
+                var mockUsers = listOf("mbaridon", "msalazar")
+                var users = preferences!!.keys.toList()
+                if(users != mockUsers){
+                    var waitingForParticipantsFragment = WaitingForParticipantsFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.results_container, waitingForParticipantsFragment).commitNow()
+                }else{
+                    var resultsFragment = MoviesResultsFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.results_container, resultsFragment).commitNow()
+                }
             }
         })
-    }
-
-    fun getMostRepeatedWord(words: List<String?>): String? {
-        return words.groupingBy{ it }.eachCount().entries.maxByOrNull{ (k, v) -> v }!!.key
     }
 }
